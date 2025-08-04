322e951b33abe5476ccfb40e2840950a26d9f16d
jeonyul <jeonyul00@gmail.com>
2025-08-04 21:14:59 +0900

    [App] set: lint && prettier && env

--- KLP_App/.prettierrc.js
+++ KLP_App/.prettierrc.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  printWidth: 150, // 모바일 화면 기반이므로 너무 길지 않게
+  tabWidth: 2, // JS/TS + RN 표준
+  useTabs: false, // 스페이스 사용
+  semi: true, // 세미콜론 붙이기
+  singleQuote: true, // 작은 따옴표 선호
+  trailingComma: 'all', // 맨 끝 쉼표 허용 (diff 깔끔)
+  bracketSpacing: true, // { foo: bar } 스타일 유지
+  arrowParens: 'avoid', // 단일 매개변수 괄호 생략
+  endOfLine: 'auto',
 };
