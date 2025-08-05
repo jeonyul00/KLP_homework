59314c02476250151492f3504afde5e596ef40d7
jeonyul <jeonyul00@gmail.com>
2025-08-05 15:11:32 +0900

    [Server] feat: 서버 최초 호출 시 토큰 유효성 검사

--- KLP_Server/controllers/auth.js
+++ KLP_Server/controllers/auth.js
@@ -5,6 +5,30 @@ const pool = require('../utils/db.js');
 const { sign } = require('../utils/jwt');
 const { validateAccountId, validatePassword, validateNickname } = require('../utils/vaildation.js');
 
+// 최로 호출
+exports.initCheck = async (req, res) => {
+    const connection = await pool.getConnection();
+    let memberId = 'unknown';
+    try {
+        const { id } = req.memberInfo;
+        if (!id) {
+            return res.json({ status: 400, message: '토큰 정보가 올바르지 않습니다.' });
+        }
+        await connection.beginTransaction();
+        const [[row]] = await connection.query(`select idx, nickname, thumbnail from member where del = "N" and idx = ?`, [id]);
+        memberId = row.idx;
+        const accessToken = await sign({ id: row.idx }, 'accessToken');
+        await connection.commit();
+        return res.json({ status: 201, message: 'hello', accessToken, id: row.idx, nickname: row.nickname, thumbnail: row.thumbnail });
+    } catch {
+        await logErrorToDB({ pk_id: memberId, request_url: req.originalUrl, payload: req.body, message: e.stack || e.message || 'unknown error' });
+        await connection.rollback();
+        return res.status(500).json({ status: 500, message: '서버 내부 오류가 발생했습니다.' });
+    } finally {
+        connection.release();
+    }
+};
+
 // 회원 가입
 exports.signup = async (req, res) => {
     const connection = await pool.getConnection();
@@ -21,7 +45,7 @@ exports.signup = async (req, res) => {
         if (nicknameError || idError || pwdError) {
             return res.json({ status: 400, message: '유효성 검사에 실패했습니다.' });
         }
-        memberId = id;
+
         await connection.beginTransaction();
         const [existing] = await connection.query('SELECT member_id FROM member WHERE member_id = ? AND del = "N"', [id]);
         if (existing.length > 0) {
@@ -29,12 +53,14 @@ exports.signup = async (req, res) => {
         }
         const hashedPassword = await bcrypt.hash(pwd, 10);
         const [result] = await connection.query(`INSERT INTO member (member_id, member_pwd, nickname, thumbnail) VALUES (?, ?, ?, ?)`, [id, hashedPassword, nickname, thumbnail]);
+        memberId = result.insertId;
         const jwtAccessToken = await sign({ id: result.insertId }, 'accessToken');
         const jwtRefreshToken = await sign({ id: result.insertId }, 'refreshToken');
         await connection.commit();
         return res.json({ status: 201, message: '가입 완료', accessToken: jwtAccessToken, refreshToken: jwtRefreshToken, id: result.insertId, nickname, thumbnail });
     } catch (e) {
         await logErrorToDB({ pk_id: memberId, request_url: req.originalUrl, payload: req.body, message: e.stack || e.message || 'unknown error' });
+        await connection.rollback();
         return res.status(500).json({ status: 500, message: '서버 내부 오류가 발생했습니다.' });
     } finally {
         connection.release();
--- KLP_Server/routes/auth.js
+++ KLP_Server/routes/auth.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 const controller = require('../controllers/auth.js');
 const { uploadImage } = require('../utils/S3.js');
+const { checkToken } = require('../utils/checkToken.js');
 
+router.post('/init', checkToken, controller.initCheck);
 router.post('/signup', uploadImage, controller.signup);
 
 module.exports = router;
