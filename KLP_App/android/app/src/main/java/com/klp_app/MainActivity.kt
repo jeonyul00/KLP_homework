683851d0a8f16e4890d13acddf0bc96a75e5d47d
jeonyul <jeonyul00@gmail.com>
2025-08-07 18:16:52 +0900

    [App] feat: splash

--- KLP_App/android/app/src/main/java/com/klp_app/MainActivity.kt
+++ KLP_App/android/app/src/main/java/com/klp_app/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.klp_app
 
+// ++
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+// ++
+import android.content.pm.ActivityInfo
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +23,11 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {    
+    // ++
+    super.onCreate(savedInstanceState)
+    setTheme(R.style.AppTheme)
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+  }
 }
